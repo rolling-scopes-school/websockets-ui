c179199e173766db58d92b23315b7347f490fb99
Andrei Auchynnikau <kele6ra@gmail.com>
2022-06-16 16:01:06 +0300

    [fix] change default port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {httpServer} from './src/http_server/index.js';
 
-const HTTP_PORT = 8181;
+const HTTP_PORT = 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
