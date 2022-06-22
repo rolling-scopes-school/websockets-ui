c58e4194a6488f79aff94a51cabdf8bbc1848c07
Andrei Ihnatsiuk <andrei_ihnatsiuk@epam.com>
2022-06-22 09:49:55 +0300

    remote-control: init structure commit

--- index.js
+++
@@ -1,9 +0,0 @@
-import Jimp from 'jimp';
-import {httpServer} from './src/http_server/index.js';
-import robot from 'robotjs';
-import { WebSocketServer } from 'ws';
-
-const HTTP_PORT = 3000;
-
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
-httpServer.listen(HTTP_PORT);
---
+++ index.ts
@@ -0,0 +1,9 @@
+import Jimp from 'jimp';
+import {httpServer} from './src/http_server/index.js';
+import robot from 'robotjs';
+import { WebSocketServer } from 'ws';
+
+const HTTP_PORT = 3000;
+
+console.log(`Start static http server on the ${HTTP_PORT} port!`);
+httpServer.listen(HTTP_PORT);
