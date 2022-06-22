3caed9990804f5a08d0fe45193c6d98fdf2e38fa
Alexey Krukovich <aliaksei.krukovich@itechart-group.com>
2022-06-22 15:45:29 +0300

    feat: add stream API to project

--- index.ts
+++ index.ts
@@ -1,22 +1,7 @@
 import { httpServer } from './src/http_server';
-import WebSocket from 'ws';
 import 'dotenv/config';
 
 const HTTP_PORT: string = process.env.FRONT_PORT;
-const SERVER_URL: string = `${process.env.WEB_SOCKET_URL}:${process.env.SERVER_PORT}`;
 
 httpServer.listen(HTTP_PORT);
 
-const socket: WebSocket = new WebSocket(SERVER_URL);
-
-socket.onerror = (error): void => {
-  console.log(error.message);
-};
-
-socket.onopen = (): void => {
-  console.log('Connection opened...');
-};
-
-socket.onmessage = (event): void => {
-  console.log(`[message] Data received from the server: ${event}`);
-};
--- server/index.ts
+++ server/index.ts
@@ -1,21 +1,29 @@
-import { WebSocketServer, WebSocket } from 'ws';
+import { WebSocketServer, createWebSocketStream } from 'ws';
 import { commandSwitcher } from '../commandSwitcher';
 import { prepareCommands } from '../utils';
 import { IWS } from '../interfaces';
 import 'dotenv/config';
 
-const wws: WebSocket = new WebSocketServer({
+const wws = new WebSocketServer({
   port: process.env.SERVER_PORT,
 });
 
-wws.on('connection', (ws: IWS): void => {
-  ws.on('message', (data: Buffer) => {
+wws.on('connection', (ws: IWS) => {
+  console.log('client connected');
+
+  const duplex = createWebSocketStream(ws);
+
+  duplex.on('data', (data: Buffer) => {
     if (Buffer.isBuffer(data)) {
       commandSwitcher({ ...prepareCommands(data.toString()), ws });
     }
   });
-});
 
-wws.on('close', () => {
-  console.log('close');
+  duplex.on('close', () => {
+    console.log('the duplex channel has closed');
+  });
+
+  ws.on('close', () => {
+    console.log('error');
+  });
 });
