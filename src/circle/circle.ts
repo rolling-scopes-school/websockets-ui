d970f742db6569e8e27d6c5e7fcb0b0e1944d7f3
Krukovich Alexey <aliaksei.krukovich@itechart-group.com>
2022-06-24 00:39:32 +0300

    fix: fix logic for draw circle

--- src/circle/circle.ts
+++ src/circle/circle.ts
@@ -10,8 +10,9 @@ class Circle {
 
   drawCircle(radius: number): void {
     const mousePos: IMousePosition = this.robot.getMousePos();
-    this.robot.mouseToggle(MOUSE.DOWN);
     this.robot.setMouseDelay(DELAY.DEFAULT);
+    this.robot.moveMouse(mousePos.x + radius, mousePos.y);
+    this.robot.mouseToggle(MOUSE.DOWN);
 
     for (let i = 0; i <= Math.PI * 2; i += 0.01) {
       const x = mousePos.x + radius * Math.cos(i);
