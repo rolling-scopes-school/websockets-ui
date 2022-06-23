147ea1fd88240413309a9a5887ed3cdf11ef4335
ungvert <pavel.chelyapin@gmail.com>
2022-06-23 23:28:36 +0300

    reafactor: circle - changed start direction

--- src/commands/drawing.ts
+++ src/commands/drawing.ts
@@ -19,18 +19,19 @@ export function drawRectangle(length: number, width: number) {
   robot.mouseToggle("up");
 }
 export function drawCircle(radius: number) {
-  robot.mouseToggle("down");
   const { x, y } = robot.getMousePos();
+  robot.mouseToggle("down");
 
-  const centerX = x - radius;
+  const centerX = x + radius;
   const centerY = y;
   const points = [];
-  const step = 0.1;
+
+  const step = 0.02;
 
   for (let a = 0; a < 2 * Math.PI + step; a += step) {
     let point: { x?: number; y?: number } = {};
-    point.x = centerX + radius * Math.cos(a);
-    point.y = centerY + radius * Math.sin(a);
+    point.x = centerX - radius * Math.cos(a);
+    point.y = centerY - radius * Math.sin(a);
     points.push(point);
     robot.moveMouseSmooth(point.x, point.y);
   }
