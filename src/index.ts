179cb7281e57e700959b25dc7c0bf6d1f00ada25
alerkasun <sandulovve@gmail.com>
2022-06-25 00:13:39 +0300

    Add ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+console.log('Hello world!suka')
 import Jimp from 'jimp';
-import {httpServer} from './src/http_server/index.js';
+import {httpServer} from './http_server';
 import robot from 'robotjs';
-import { WebSocketServer } from 'ws';
+// import { WebSocketServer } from 'ws';
 
 const HTTP_PORT = 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
