be1a955a204d65f6d26acced8dee70dd13b97cc0
User <belpc2012@gmail.com>
2022-07-02 15:34:33 +0300

    feat: create web socket

--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var index_js_1 = require("./src/http_server/index.js");
-var HTTP_PORT = 3000;
-var WS_PORT = 8080;
-console.log("Start static http server on the ".concat(HTTP_PORT, " port!"));
-index_js_1.httpServer.listen(HTTP_PORT);
--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
 import Jimp from 'jimp';
 import {httpServer} from './src/http_server/index.js';
 import robot from 'robotjs';
-import { WebSocketServer } from 'ws';
+import ws from 'ws';
+import { WebSocketServer, createWebSocketStream } from 'ws';
 
 const HTTP_PORT = 3000;
 const WS_PORT = 8080;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
+
+const wsServer = new WebSocketServer({ port : WS_PORT});
+wsServer.on('connection', (ws: ws.WebSocket) => {
+    console.log(`Web socket server on the ${WS_PORT} port!`);
+    ws.on('message', (data: ws.RawData) => {
+    const duplex = createWebSocketStream(ws, { encoding: 'utf-8', decodeStrings: false });
+    });
+
+})
 httpServer.listen(HTTP_PORT);
