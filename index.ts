646d6766f717cbdb6edc6f07268047e9de032082
Ekaterina Zaharina <katia.zakharina@gmail.com>
2023-01-22 11:38:13 +0300

    feat: configure dotenv

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import { httpServer } from './src/http_server/index';
 import mouse from '@nut-tree/nut-js';
+import 'dotenv/config';
 
-const HTTP_PORT = 8181;
+const port = Number(process.env.HTTP_PORT) || 3000;
 
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
-httpServer.listen(HTTP_PORT);
+console.log(`Start static http server on the ${port} port!`);
+httpServer.listen(port);
