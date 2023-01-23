6ff88e4743957562587a2c7a3ab2554e4d059e5a
nevmerzhitskaya <lamara@tut.by>
2023-01-23 23:40:59 +0100

    feat: add constats

---
+++ src/app/constants.ts
@@ -0,0 +1,20 @@
+// export const enum StatusCode {
+//     OK = 200,
+//     BAD_REQUEST = 400,
+//     NOT_FOUND = 404,
+//     CREATED = 201,
+//     NO_CONTENT = 204,
+//     SERVER_ERROR = 500
+// }
+
+export const enum Commands {
+    mouse_up = 'mouse_up',
+    mouse_down = 'mouse_down',
+    mouse_left = 'mouse_left',
+    mouse_right = 'mouse_right',
+    mouse_position = 'mouse_position',
+    draw_circle='draw_circle',
+    draw_rectangle='draw_rectangle',
+    draw_square='draw_square',
+    prnt_scrn='prnt_scrn'
+}
