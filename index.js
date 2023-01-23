4b8e951928772bfc4c4020478eafa2bcc5ce88c6
Tema_sevastopol <temasebastopol@yandex.ru>
2023-01-23 23:04:52 +0300

    feat: started

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import { httpServer } from "./src/http_server/index.js";
-import { mouse } from "@nut-tree/nut-js";
+// import { mouse } from "@nut-tree/nut-js";
 
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
