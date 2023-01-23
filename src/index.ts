1bffca2f25022d003eb82a4ad62e9da93cab302e
Mikhail Sigarev <mikhail_sigarev@epam.com>
2023-01-24 02:23:32 +0300

    add typescript

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { httpServer } from "./src/http_server/index.js";
-import { mouse } from "@nut-tree/nut-js";
+import { httpServer } from "./http_server";
+// import { mouse } from "@nut-tree/nut-js";
 
 const HTTP_PORT = 8181;
 
