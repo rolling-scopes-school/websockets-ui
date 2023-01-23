9615b5ce4abb88e185f29c4e216beb3bad6cc567
Mikhail Sigarev <mikhail_sigarev@epam.com>
2023-01-24 02:25:06 +0300

    implement navigation

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -1,8 +1,12 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as http from 'http';
+import { WebSocketServer } from 'ws';
+import { mouse, Point } from "@nut-tree/nut-js";
+// import { mouse, Point, Button } from "@nut-tree/nut-js";
 
 export const httpServer = http.createServer(function (req, res) {
+    console.log('req', req.url);
     const __dirname = path.resolve(path.dirname(''));
     const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
     fs.readFile(file_path, function (err, data) {
@@ -14,4 +18,64 @@ export const httpServer = http.createServer(function (req, res) {
         res.writeHead(200);
         res.end(data);
     });
+});
+
+const wss = new WebSocketServer({ port: 5000 });
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', async function message(data) {
+        console.log('received: ', data);
+        let position = await mouse.getPosition();
+        console.log('position', position);
+        const array = data.toString().split(' ');
+        console.log('array', array[0], array[1]);
+        console.log('bool', array[0] === 'mouse_up');
+        if (array[0] === 'mouse_right') {
+            position = new Point(position.x + Number(array[1]), position.y);
+            console.log('before', position);
+            await mouse.move([position]);
+            console.log('after');
+            ws.send(`mouse_right ${position.x},${position.y}`);
+        }
+        if (array[0] === 'mouse_left') {
+            position.x = position.x -  Number(array[1]);
+            await mouse.move([position]);
+            ws.send(`mouse_left ${position.x},${position.y}`);
+        }
+        if (array[0] === 'mouse_up') {
+            position.y = position.y -  Number(array[1]);
+            await mouse.move([position]);
+            ws.send(`mouse_up ${position.x},${position.y}`);
+        }
+        if (array[0] === 'mouse_down') {
+            position.y = position.y +  Number(array[1]);
+            await mouse.move([position]);
+            ws.send(`mouse_down ${position.x},${position.y}`);
+        }
+        if (array[0] === 'mouse_position') {
+            ws.send(`mouse_position ${position.x},${position.y}`);
+        }
+        // if (array[0] === 'draw_rectangle') {
+        //     ws.send(`draw_rectangle`);
+        // }
+        // if (array[0] === 'draw_circle') {
+        //     ws.send(`draw_circle`);
+        // }
+        // if (array[0] === 'draw_square') {
+        //     await mouse.pressButton(Button.LEFT);
+        //     await mouse.move([
+        //         position,
+        //         new Point(position.x + Number(array[1]), position.y),
+        //         new Point(position.x + Number(array[1]), position.y + Number(array[1])),
+        //         new Point(position.x, position.y + Number(array[1])),
+        //         position,
+        //     ]);
+        //     await mouse.releaseButton(Button.LEFT);
+        //     ws.send(`draw_square`);
+        // }
+    });
+
+    console.log('connection received: %s');
+
+    ws.send('something');
 });
