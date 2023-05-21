de047ee492e75f4517bc0055ebcc4e5509e5b9ea
AdiletMaksatuly <bakeryamaker1@gmail.com>
2023-05-22 03:28:02 +0600

    feat: implement correct program termination

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,25 @@ const httpServer = startHttpServer(HTTP_PORT, () => {
     console.log(`Started static http server on the ${HTTP_PORT} port.`);
 });
 
-const wss = startWebSocket(WS_PORT);
+const wsServer = startWebSocket(WS_PORT);
 
+
+const handleSignal = (signal: string, code: number) => {
+    console.log('Closing HTTP and WebSocket servers...')
+    console.log(`Got ${signal} signal with code ${code}.`)
+
+    wsServer.clients.forEach((client) => {
+        client.close();
+    });
+
+    wsServer.close();
+
+    httpServer.close();
+
+    process.exit(code);
+}
+
+process.on('SIGINT', handleSignal);
+process.on('SIGQUIT', handleSignal);
+process.on('SIGTERM', handleSignal);
+process.on('uncaughtException', handleSignal);
