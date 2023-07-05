9a2b1ebe970dd053ea55c09910ad9053101a7f32
andrew-Go1 <deletecrack@gmail.com>
2023-07-05 18:43:53 +0000

    init: battleship-task init

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+import { WebSocket } from "ws";
 import { httpServer } from "./src/http_server/index.js";
-
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const ws = new WebSocket;
+let socket = new ws.Server ('ws://localhost:3000');
+
+socket.onmessage = event => console.log(event.data);
+socket.onerror = error => console.log(error);
