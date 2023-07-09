c5b8297fbcef89a24d2fbe0c432d492098b18295
Anatolii Pokhylko <pokhylko@gmail.com>
2023-07-09 12:04:29 +0300

    feat: add dependencies and settings

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import { httpServer } from "./src/http_server/index.js";
+import { httpServer } from "./src/http_server/index";
+import { wsServer } from "./src/ws_server/index.ts";
 
 const HTTP_PORT = 8181;
+const WS_PORT = 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+wsServer(WS_PORT);
