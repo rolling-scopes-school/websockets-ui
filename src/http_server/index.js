23070dcb5703232b9b5e8377b749f0bbdd818606
Serg <martynenko1981@gmail.com>
2023-07-18 00:20:48 +0300

    feat: add chapter 0

--- src/http_server/index.js
+++ src/http_server/index.js
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as http from 'http';
-
+//
 export const httpServer = http.createServer(function (req, res) {
     const __dirname = path.resolve(path.dirname(''));
     const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
