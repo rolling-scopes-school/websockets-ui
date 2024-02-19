c4c1d34e61e5fd0c150a2afe736c3cae516d7641
malDev09 <159049731+malDev09@users.noreply.github.com>
2024-02-19 14:14:17 +0300

    feat: ts implement

--- src/websocket/websocket-server.ts
+++ src/websocket/websocket-server.ts
@@ -1,18 +1,22 @@
 import { WebSocketServer } from "ws";
-import gameData from "./game-data.js";
+import * as gameData from "./game-data.js";
+
+let playerID = 1;
 
 const wss = new WebSocketServer({ port: 3000 });
 
 console.log("WebSocket server started");
 
-function sendMessage(ws, message) {
-  ws.send(JSON.stringify(message));
+function sendMessage(ws: any, message: any) {
+  ws.send(message);
 }
 
-wss.on("connection", function connection(ws) {
+wss.on("connection", function connection(ws: any) {
   console.log("New client connected");
+  ws.id = playerID++;
+  ws.on("error", console.error);
 
-  ws.on("message", function incoming(message) {
+  ws.on("message", function incoming(message: string) {
     console.log("received: %s", message);
     try {
       const jsonMessage = JSON.parse(message);
@@ -30,7 +34,7 @@ wss.on("connection", function connection(ws) {
   });
 });
 
-function handleIncomingMessage(ws, message) {
+function handleIncomingMessage(ws: any, message: any) {
   switch (message.type) {
     case "reg":
       const regData = JSON.parse(message.data);
@@ -39,32 +43,33 @@ function handleIncomingMessage(ws, message) {
     case "create_room":
       handleCreateRoom(ws, message.data);
       break;
-    case "add_user_to_room":
-      handleAddUserToRoom(ws, message.data);
-      break;
+    // case "add_user_to_room":
+    //   handleAddUserToRoom(ws, message.data);
+    //   break;
     default:
       sendMessage(ws, { type: "error", message: "Unknown command" });
   }
 }
 
-function handleRegistration(ws, data) {
+function handleRegistration(ws: any, data: any) {
   const { name, password } = data;
   const player = { name, password };
   gameData.players.push(player);
-
-  sendMessage(ws, {
-    type: "reg",
-    data: {
-      name: name,
-      index: gameData.players.length - 1,
-      error: false,
-      errorText: "",
-    },
+  const dataUser = JSON.stringify({
+    name: name,
+    index: 1,
+    error: false,
+    errorText: '',
+  })
+  const responseData =  JSON.stringify({
+    type: 'reg',
+    data: dataUser,
     id: 0,
-  });
+  })
+  sendMessage(ws,responseData);
 }
 
-function handleCreateRoom(ws, data) {
+function handleCreateRoom(ws: any, data: any) {
   const room = {
     id: gameData.rooms.length + 1,
     players: [{ ws, index: data.index }],
@@ -77,15 +82,19 @@ function handleCreateRoom(ws, data) {
   });
 }
 
-function handleAddUserToRoom(ws, data) {
-  const room = gameData.rooms.find((room) => room.id === data.indexRoom);
-  if (room) {
-    room.players.push({ ws, index: gameData.players.length });
-    room.players.forEach((player) =>
-      sendMessage(player.ws, {
-        type: "update_room",
-        data: [{ roomId: room.id, roomUsers: room.players }],
-      })
-    );
-  }
+// function handleAddUserToRoom(ws: any, data: any) {
+//   const room = gameData.rooms.find((room) => room.id === data.indexRoom);
+//   if (room) {
+//     room.players.push({ ws, index: gameData.players.length });
+//     room.players.forEach((player) =>
+//       sendMessage(player.ws, {
+//         type: "update_room",
+//         data: [{ roomId: room.id, roomUsers: room.players }],
+//       })
+//     );
+//   }
+// }
+
+function handlePlayerDisconnect(ws: any) {
+  // Your logic for handling player disconnect
 }
