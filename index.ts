9fe96cb107bdcfe3d03d8d922ab2e768334eefe5
Arelkench <nefera2801@gmail.com>
2024-02-25 19:09:28 +0100

    bug fix

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { httpServer } from './src/http_server';
 import { wss } from './src/server/websocket';
 import { Server } from './src/server';
 
-const HTTP_PORT = process.env.PORT || 8000;
+const HTTP_PORT =  3000;
 
 console.log(`Http server started on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
