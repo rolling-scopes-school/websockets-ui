659bd3ecfabe628f497b6b307ef7ea4a8989cfaa
Volha Soika <Volha_Soika@epam.com>
2024-02-25 13:27:02 +0200

    feat: dependencies setup

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', '@typescript-eslint'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/typescript'
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  'prettier/prettier': [
+    'error',
+    {
+      endOfLine: 'auto',
+    },
+  ],
+  settings: {
+    'import/resolver': {
+      foo: { someConfig: value }
+    }
+  },
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+};
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import { httpServer } from "./src/http_server/index.js";
+import { httpServer } from './src/http_server/index';
 
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
+
 httpServer.listen(HTTP_PORT);
