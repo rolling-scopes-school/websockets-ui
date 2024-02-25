24b2cf4108f7e15298818fb1336c79ebc575fbb3
Arelkench <nefera2801@gmail.com>
2024-02-25 19:08:43 +0100

    add env config

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
+import "dotenv/config";
 import { httpServer } from './src/http_server';
 import { wss } from './src/server/websocket';
 import { Server } from './src/server';
 
-const HTTP_PORT = 8000;
+const HTTP_PORT = process.env.PORT || 8000;
 
 console.log(`Http server started on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
